cc2479b2f4ca44efab1fa8d9f395f2219307cd13
CodeSmash <118036145+Somesh2672@users.noreply.github.com>
2023-07-30 01:12:47 +0530

    Update script.js

--- profile/script.js
+++ profile/script.js
@@ -1,9 +1,17 @@
 const fName = document.getElementById('fName');
 const lName = document.getElementById('lName');
 const emailId = document.getElementById('emailID');
+const password = document.getElementById('password');
+const logOut = document.getElementById('logOut');
 
-let currentUser = JSON.parse(sessionStorage.getItem('loggenInUser'));
+
+let currentUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
 
 fName.innerText = currentUser.firstName;
 lName.innerText = currentUser.lastName;
 emailId.innerText = currentUser.email;
+password.innerText = currentUser.password;
+
+logOut.addEventListener('click',()=>{
+    location.href='../login';
+})
